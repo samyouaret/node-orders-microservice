c8d27f4a550c4af40e110f3919500d75d0392a63
samyouaret <samyouaret13@gmail.com>
2022-12-01 00:25:13 +0100

    Update the db volume and env names

--- orders.js
+++ orders.js
@@ -7,9 +7,9 @@ app.use(express.urlencoded({ extended: false }))
 
 const client = new Client({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
 })
 
 async function connect() {
