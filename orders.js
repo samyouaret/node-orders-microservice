afcd6d7952111b98462809309084f69da475d43a
samyouaret <samyouaret13@gmail.com>
2022-12-01 23:59:29 +0100

    Add Kafka a broker for events exchaning

--- orders.js
+++ orders.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const { Client } = require('pg')
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+    clientId: 'orders-app',
+    brokers: ['broker:29092'],
+})
+const producer = kafka.producer()
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-const status = {
-    PENDING: "PENDING",
-}
 
 const client = new Client({
     host: process.env.DB_HOST,
@@ -17,7 +21,11 @@ const client = new Client({
     database: "postgres"
 })
 
-async function connect() {
+const status = {
+    PENDING: "PENDING",
+}
+
+async function connect_db() {
     await client.connect()
     const res = await client.query('SELECT $1::text as message', ['postgres connected!'])
     console.log(res.rows[0].message) // postgres connected!
@@ -30,6 +38,21 @@ app.get("/orders", async (req, res) => {
     res.json(result.rows)
 });
 
+const consumer = kafka.consumer({ groupId: 'test-group' });
+app.get("/events", async (req, res) => {
+    let events = [];
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            events.push({
+                partition,
+                offset: message.offset,
+                value: message.value.toString(),
+            });
+        },
+    })
+    res.json(events)
+});
+
 app.post("/orders", async (req, res) => {
     const result = await client.query("INSERT INTO public.orders(status) values($1) RETURNING *", [status.PENDING]);
     const order = result.rows[0];
@@ -38,6 +61,24 @@ app.post("/orders", async (req, res) => {
     }
     // publish product OrderCreated 
     // {order_id, products[{id, qty}]}
+    let payload = Buffer.from(JSON.stringify({
+        type: "ORDER_CREATED",
+        payload: {
+            orderId: order.id,
+            products: req.body.products
+        }
+    }));
+
+    try {
+        await producer.send({
+            topic: 'orders',
+            messages: [
+                { value: payload },
+            ],
+        })
+    } catch (error) {
+        console.error(error);
+    }
     res.json({
         ...order,
         products: req.body.products
@@ -48,5 +89,22 @@ app.use((err, req, res) => {
     res.json(err.message);
 });
 
-connect()
-app.listen(PORT, () => console.log(`App on port: ${PORT}`))
+async function init() {
+    await connect_db()
+    await producer.connect()
+    // Consuming
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'orders', fromBeginning: true })
+    app.listen(PORT, () => console.log(`App on port: ${PORT}`))
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                partition,
+                offset: message.offset,
+                value: message.value.toString(),
+            })
+        },
+    })
+}
+
+init();
